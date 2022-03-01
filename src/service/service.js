86bf9fa845aa9207225fb67907f77c5684334102
Lluís <lluisagustisan@gmail.com>
2022-03-01 14:08:11 +0100

    api services working character by name / id

---
+++ src/service/service.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const api = "https://breakingbadapi.com/api";
+
+export const getAllCharacters = async () => {
+  return await axios.get(`${api}/characters`);
+};
+
+export const getCharacterDataByName = async (name) => {
+  return await axios.get(`${api}/characters?name=${name}`);
+};
+
+export const getCharacterDataById = async (id) => {
+  return await axios.get(`${api}/characters/${id}`);
+};
+
+export const getCharacterQuote = async (name) => {
+  return await axios.get(`${api}/quote/random?author=${name}`);
+};
